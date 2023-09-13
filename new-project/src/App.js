d9263df5af435984ac2667b8264321e9124fa1f5
Douglas.Y <siyuan.yang2@uq.net.au>
2023-09-13 14:25:20 +1000

    break_env: audit fix caused bug

---
+++ new-project/src/App.js
@@ -0,0 +1,12 @@
+import Home from 'Plugins/Map/Map'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './plugins/map/Map';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
