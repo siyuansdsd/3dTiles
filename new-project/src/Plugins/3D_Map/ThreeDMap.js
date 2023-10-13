fe04a3d1e84e4c1fc38a3e6f9bd9d5d6ba3eee63
Douglas.Y <siyuan.yang2@uq.net.au>
2023-10-13 18:21:12 +1000

    this is the third try

--- new-project/src/Plugins/3D_Map/ThreeDMap.js
+++ new-project/src/Plugins/3D_Map/ThreeDMap.js
@@ -29,7 +29,7 @@ const mapOptions = {
 };
 
 const ThreeDMap = () => {
-    return <><Wrapper apiKey={ process.env.REACT_APP_API_KEY }><MyMap /></Wrapper></>
+    return (<><Wrapper apiKey={ process.env.REACT_APP_API_KEY }><MyMap /></Wrapper></>)
 }
 
 function MyMap () {
